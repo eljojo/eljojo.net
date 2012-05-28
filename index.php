2919c30f8ca01a85bed9384d4cbdd830eff82e17
José Tomás Albornoz <jojo@eljojo.net>
2012-05-27 21:53:42 -0400

    primer commit

--- index.php
+++ index.php
@@ -1,81 +1,84 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+$elegido = mt_rand(0,1);
+$color = ($elegido == 0) ? 'oscuro' : 'claro';
+?><!DOCTYPE html>
+<html lang="es">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8" />
 	<title>Jos&eacute; Tom&aacute;s Albornoz</title>
-	<meta content="es" http-equiv="Content-Language" />
-	<meta content="17 a&ntilde;os, programador, ser humano." name="description" />
-	<meta name="keywords" content="jojo, eljojo, jose, tomas, albornoz, rodriguez, JOjoDesigns, eljojo2, mychemicaljojo, purininis, ajipirijou, el, jojo, www.purininis.com, purininis.com, ajipirijou.com, donsaludador, jojobots, dondespedidor, sraclima" />
+	<meta content="18 a&ntilde;os, programador, ser humano." name="description" />
+	<meta name="keywords" content="jojo, eljojo, jose, tomas, albornoz, rodriguez, JOjoDesigns, eljojo2, mychemicaljojo, purininis, ajipirijou, el, jojo, www.purininis.com, purininis.com,eljojo.net, ajipirijou.com, donsaludador, jojobots, dondespedidor, sraclima" />
+	<link rel="canonical" href="http://josealbornoz.cl/" />
 	<meta property="og:title" content="Jos&eacute; Tom&aacute;s Albornoz"/>
-	<meta property="og:description" content="17 a&ntilde;os, programador, ser humano." />
+	<meta property="og:description" content="18 a&ntilde;os, programador, ser humano." />
 	<meta property="og:type" content="public_figure"/>
-	<meta property="og:url" content="http://eljojo.net/"/>
-	<meta property="og:image" content="cdn/yo/yo_1_1.jpg"/>
+	<meta property="og:url" content="http://josealbornoz.cl/"/>
+	<meta property="og:image" content="http://perfectirijillo.com/eljojo.net/yo/yo_1_1.jpg"/>
 	<meta property="fb:admins" content="586872350"/>
-	<meta property="og:email" content="jojo@eljojo.net"/>
-	<link href="cdn/2011-02-20/estilo.css" media="screen, projection" rel="stylesheet" type="text/css" />
-	<link rel="shortcut icon" href="cdn/2011-02-20/favicon.png" />
-	<script src="cdn/2011-08-15/twitter.js" type="text/javascript"></script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript"></script>
-	<script src="cdn/2011-08-15/js.js" type="text/javascript"></script>
+	
+	<link href="estilo.css?v=1.1" media="screen, projection" rel="stylesheet" type="text/css" />
+	<meta name="viewport" content = "width=850,height=device-height, initial-scale = 0.55, minimum-scale = 0.55, maximum-scale = 1.5" />  
+	<link rel="shortcut icon" href="yo.png" />
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+	<script>
+		var cargado = function(){
+			$(window).resize(resize);
+			resize();
+			$("<img/>").attr('src','fondo.jpg').load(function(e) {
+				if(e.type == 'load') $("body").css('background-image', 'url(fondo.jpg)');
+			});
+		}
+		var resize = function(){
+			var v = $(this); //ventana
+			var a = 853 *v.width() /  1280;
+			if(Math.floor(v.height() - a)>0) {
+				$("body").css('background-size','auto '+v.height()+'px');
+			}else{
+				$("body").css('background-size','cover')
+			}
+		}
+		$(cargado);
+	</script>
 </head>
-
-<body>
-<div id="separador">
-	<div id="contenedor">
-		<div id="header">
-			<a href="/"><img src="cdn/2011-02-20/logo.png" width="484" height="286" id="eljojo" alt="elJOjo" /></a>
-			<div id="derecha"><h1>es Jos&eacute; Tom&aacute;s Albornoz</h1>
-				<div id="twitter"></div>
-			</div>
-		</div>
-		<div id="contenido">
-			<img src="cdn/yo/yo6.jpg" width="384" height="256" id="yo" alt="Jos&eacute; Tom&aacute;s Albornoz" />
-			<h1>Programador y Ser Humano<small>todo al mismo tiempo</small></h1>
-			<p>
-				Nac&iacute; (y vivo) en <strong>Chill&aacute;n, Chile.</strong><br />
-				Tengo <strong>17 a&ntilde;os</strong> y estoy en cuarto medio.<br />
-				<strong>Uso</strong> <span style="text-decoration: line-through;">Linux</span> <strong>Mac</strong> y no le temo a la consola.<br />
-				Programo en <strong>PHP/MySQL</strong>, hago sitios web.<br />
-				Hago <a href="http://bots.eljojo.net"><strong>bots de twitter</strong></a> que son amistosos.<br />
-				Mi blog es <a href="http://ajipirijou.com">&iexcl;Ajipirijou!</a> 
-				y mi tumblr es <a href="http://www.purininis.com">Purininis</a>.<br/>
-				Mi curr&iacute;culum lo puedes ver <a href="cv">ac&aacute;</a>.
-			</p>
-		</div>
-		<div id="redes">
-			<h2>Estoy en las siguientes Redes Sociales</h2>
-			<a href="http://twitter.com/eljojo"><img class="margen" src="cdn/twitter.png" width="130" height="130" alt="Mi Twitter" /></a>
-			<a href="http://facebook.com/eljojo"><img class="margen" src="cdn/facebook.png"  width="130" height="130" alt="Mi Facebook" /></a>
-			<a href="http://delicious.com/eljojo2"><img class="margen" src="cdn/delicious.png" width="130" height="130" alt="Mi Delicious"/></a>
-			<a href="http://www.last.fm/user/eljojo_net"><img class="margen" src="cdn/last.png" width="130" height="130" alt="Mi Last.fm"/></a>
-			<a href="http://flickr.com/photos/eljojo"><img src="cdn/flickr.png" width="130" height="130" alt="Mi Flickr"/></a>
-		</div>
-		<div id="mail">
-			<img src="cdn/mail.png" width="111" height="80" alt="M&aacute;ndame un mail"/>
-			<h2>&iquest;Me quieres Contactar?</h2>
-			Escr&iacute;beme un mail a <script type="text/javascript">
-			//<![CDATA[
-			<!--
-			var x="function f(x){var i,o=\"\",l=x.length;for(i=l-1;i>=0;i--) {try{o+=x.c" +
-			"harAt(i);}catch(e){}}return o;}f(\")\\\"function f(x,y){var i,o=\\\"\\\\\\\""+
-			"\\\\,l=x.length;for(i=0;i<l;i++){if(i<28)y++;y%=127;o+=String.fromCharCode(" +
-			"x.charCodeAt(i)^(y++));}return o;}f(\\\"\\\\ypBVHBG_\\\\\\\\003XCZARUU\\\\\\"+
-			"\\025\\\\\\\\035}\\\\\\\\\\\"\\\\e/;.+r\\\\\\\\rq94?;,6`1371\\\\\\\\037\\\\" +
-			"\\\\005\\\\\\\\r\\\\\\\\010\\\\\\\\014\\\\\\\\016\\\\\\\\nH\\\\\\\\t\\\\\\\\"+
-			"r\\\\\\\\035U\\\\\\\\030\\\\\\\\031\\\\\\\\017\\\\\\\\004\\\\\\\\n\\\\\\\\0" +
-			"23\\\\\\\\005O;\\\\\\\\033\\\\\\\\031\\\\\\\\027W26\\\\\\\\020\\\\\\\\024]!" +
-			"\\\\\\\\\\\\\\\\ ukwh`;[*ceacMkcz~x|:{scD;$qswq_EMHLNJ\\\\\\\\010IM]\\\\\\\\"+
-			"026\\\\\\\\004M\\\\\\\\023\\\\\\\\014\\\\\\\\006\\\\\\\\013\\\\\\\\001\\\\\\"+
-			"\\t\\\"\\\\,28)\\\"(f};)lo,0(rtsbus.o nruter};)i(tArahc.x=+o{)--i;0=>i;1-l=" +
-			"i(rof}}{)e(hctac};l=+l;x=+x{yrt{)77=!)31/l(tAedoCrahc.x(elihw;lo=l,htgnel.x" +
-			"=lo,\\\"\\\"=o,i rav{)x(f noitcnuf\")"                                       ;
-			while(x=eval(x));
-			//-->
-			//]]>
-			</script>
-		</div>
-	</div>
-</div>
+<body class="claro">
+	<header>
+		<h1>Jos&eacute; Tom&aacute;s Albornoz <small>(eljojo)</small></h1>
+		<h2>programador y ser humano, todo al mismo tiempo.</h2>
+	</header>
+	<section>
+		<p>
+			<span>Tengo 18 a&ntilde;os y vivo en Santiago, Chile.</span><br />
+			<span>Hago lo que me gusta, codeo sitios web y cuando me aburro juego con la API de twitter.</span><br />
+			<span>Me gusta integrar sistemas, trabajar con servidores y hacer proyectos s&oacute;lo por el hecho de hacerlos.</span>
+		</p>
+		<p>
+			<span>En la vida real me gusta <a href="http://flickr.com/photos/eljojo/">la fotograf&iacute;a</a>, escribir en <a href="http://ajipirijou.com">mi blog</a> y <a href="http://last.fm/user/eljojo_net">escuchar m&uacute;sica</a>.</span>
+		</p>
+		<p>
+			<span>En internet tengo <a href="http://twitter.com/eljojo">twitter</a>, <a href="http://www.purininis.com">tumblr</a> y <a href="http://delicious.com/eljojo2">delicious</a>.</span>
+		</p>
+	</section>
+	<footer>
+		<p>
+			<span>Mi correo es  
+				<script type="text/javascript">
+				//<![CDATA[
+				<!--
+				var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+				"=123){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return " +
+				"o.substr(0,ol);}f(\")09,\\\"t~veit(g{h1!-l.*PTQY{USW]010\\\\720\\\\hGW_630\\"+
+				"\\@DBFGOiGMIO600\\\\771\\\\730\\\\DLkwi<9Fxtx~(`pw{rz}2xnd'gmiohfBnj120\\\\" +
+				"720\\\\F420\\\\610\\\\520\\\\120\\\\620\\\\330\\\\W(N420\\\\420\\\\200\\\\7" +
+				"00\\\\N410\\\\PIB700\\\\400\\\\200\\\\220\\\\410\\\\620\\\\420\\\\L520\\\\6" +
+				"10\\\\:3(?4>\\\"(f};o nruter};))++y(^)i(tAedoCrahc.x(edoCrahCmorf.gnirtS=+o" +
+				";721=%y{)++i;l<i;0=i(rof;htgnel.x=l,\\\"\\\"=o,i rav{)y,x(f noitcnuf\")"     ;
+				while(x=eval(x));
+				//-->
+				//]]>
+				</script>	
+			</span>
+		</p>
+	</footer>
 </body>
 </html>
