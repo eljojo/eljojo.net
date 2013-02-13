c9287a4279d404b719bc27075f2baa754c7c9b1c
José Tomás Albornoz <jojo@eljojo.net>
2013-02-13 21:46:57 +0100

    brought from past old eljojo.net and cv :)

---
+++ cv/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Jos&eacute; Tom&aacute;s Albornoz</title>
+		<meta name="description" content="Programador y estudiante. 17 a&ntilde;os." />
+		<meta name="keywords" content="eljojo, jose, tomas, albornoz, rodriguez, chillan, php, mysql, jojo" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js" type="text/javascript"></script>
+		<link href="http://fonts.googleapis.com/css?family=Cardo&amp;subset=latin" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="/fancybox/jquery.fancybox-1.3.3.pack.js?v=2.3"></script>
+		<script type="text/javascript" src="/javascript.js"></script>
+		<link rel="stylesheet" type="text/css" href="estilo.css" media="screen" />
+		<link rel="stylesheet" type="text/css" href="./fancybox/jquery.fancybox-1.3.3.css?v=2.3" media="screen" />
+		<link rel="shortcut icon" href="http://static.eljojo.net/nuevo/img/favicon.png" />
+		<link rel="canonical" href="http://cv.eljojo.net/" />
+		<meta content="17 a&ntilde;os, programador, ser humano." name="description" />
+		<meta name="keywords" content="jojo, eljojo, jose, tomas, albornoz, rodriguez, JOjoDesigns, eljojo2, mychemicaljojo, purininis, ajipirijou, el, jojo, www.purininis.com, purininis.com, ajipirijou.com, donsaludador, jojobots, dondespedidor, sraclima" />
+		<meta property="og:title" content="Jos&eacute; Tom&aacute;s Albornoz"/>
+		<meta property="og:description" content="17 a&ntilde;os, programador, ser humano." />
+		<meta property="og:type" content="public_figure"/>
+		<meta property="og:url" content="http://cur.eljojo.net/"/>
+		<meta property="og:image" content="http://static.eljojo.net/nuevo/img/yo2.jpg"/>
+		<meta property="fb:admins" content="586872350"/>
+		<meta property="og:email" content="jojo@eljojo.net"/>
+	</head>
+	<body>
+		<h1 class="quote">Jos&eacute; Tom&aacute;s Albornoz</h1>
+		<p class="quote">Programo desde los 14 a&ntilde;os, tengo planes para conquistar el mundo.</p>
+		<h2>&iquest;Qu&eacute; hago?</h2>
+		<p>Desde Octubre de 2010 <strong>trabajo para <a href="http://grm.cl/">Grupo de Medios Regionales</a></strong>, soy parte del Equipo Web de <a href="http://www.soychile.cl/">soychile.cl</a>.</p>
+		<p><strong>Sitios Web</strong>, los programo. Trabajo con <strong>PHP</strong> para el backend y <strong>MySQL</strong> para bases de datos. Hago que los sitios sean m&aacute;s din&aacute;micos mezclando <strong>Ajax</strong> con PHP. Hago uso intensivo de <strong>jQuery</strong>.</p>
+		<p><strong>Integro redes sociales</strong>. Trabajo con las <strong>APIs</strong> m&aacute;s importantes (<strong>Twitter</strong>, <strong>Facebook</strong>, <strong>Flickr</strong>, <strong>YouTube</strong>, incluso <strong>Dropbox</strong>). Implemento estas redes en los sitios web que hago.</p>
+		<p><strong>Bots de Twitter</strong>, hago bots de twitter capaces de establecer una <strong>conversaci&oacute;n a nivel humano</strong>. Suman m&aacute;s de <strong>42.000 followers</strong>.</p>
+		<p><strong>Proyectos creativos</strong>. Desde propuestas para <strong>reciclar computadores</strong>, pasando por el taller de rob&oacute;tica en mi colegio hasta <strong>radios on-line</strong>. He aparecido en el <strong>diario</strong> varias veces, ganado el concurso <strong>El Mercurio de los Estudiantes</strong> y <strong>dado charlas</strong>.</p>
+		<?php /*<h2>&iquest;Qu&eacute; he hecho?</h2>
+		<ul>
+		
+			$hecho = array(
+				array('Campos-Lopez.com', 'campos-lopez', 'El sitio de Francisco Campos Lopez, director Chileno.'),
+				array('MauleC.cl', 'maulec', 'Portal de las Comunicaciones de la Regi&oacute;n del Maule.'),
+				array('FreeSomeday.com', 'freesomeday', 'Compa&ntilde;&iacute;a multimedial con base en USA.'),
+				array('elJOjo.net', 'eljojo', 'Mi sitio web personal.'),
+			);
+			shuffle($hecho);
+			foreach ($hecho as $sitio) {
+				echo "<li><a class=\"foto\" href=\"/sitios/{$sitio[1]}.jpg\">{$sitio[0]}</a>: {$sitio[2]}</li>";
+			}
+		?>
+			<li><a href="http://bots.eljojo.net">Bots de Twitter</a>: Cuentas de Twitter automatizadas que pueden interactuar a nivel humano.</li>
+			<li><a class="iframe yt" href="http://www.youtube.com/embed/htjKrGYbDLY?rel=0">S&uacute;per Chancho System</a>: Sistema para convertir un computador viejo en un reproductor de m&uacute;sica usando Linux.</li>
+		</ul> */ ?>
+		<h2>Un poco m&aacute;s de mi</h2>
+		<p>Tengo 17 a&ntilde;os y vivo en Chill&aacute;n, Chile. <br />
+			Prefiero los sistemas basados en <strong>UNIX</strong>, us&eacute; <strong>Linux</strong> (como sistema de escritorio) por un poco m&aacute;s de un a&ntilde;o. Todav&iacute;a lo uso en mis servidores. Me compr&eacute; mi <strong>MacBook Pro</strong>.<br />
+			Me considero innovador y proactivo.<br />
+			Me apasiona <a href="http://www.flickr.com/photos/eljojo/">la fotograf&iacute;a</a>.<br />
+	<?php /*	Siempre me ha gustado la inform&aacute;tica. Planeo estudiar <strong>Ingenier&iacute;a Civil Inform&aacute;tica</strong> en la <strong>Universidad T&eacute;cnica Federico Santa Mar&iacute;a</strong>.<br /> */ ?>
+	</p>
+		<h2>Informaci&oacute;n de Contacto</h2>
+		<p>Mi correo es <script type="text/javascript">
+	//<![CDATA[
+	<!--
+	var x="function f(x,y){var i,o=\"\",l=x.length;for(i=0;i<l;i++){y%=127;o+=St" +
+	"ring.fromCharCode(x.charCodeAt(i)^(y++));}return o;}f(\";+1\\003\\025\\013\\"+
+	"014\\nE\\000O\\020@\\021\\035\\r\\037N\\006\\\\\\036OQVY\\031\\033E\\001T\\" +
+	"027\\031\\023\\031ti.o9jj<\\177acgi%v!sysaWzrrYm2w3,-6\\001\\034\\027\\032\\"+
+	"r^RUQRR\\000\\021U\\025C\\033\\014^\\010IVWC[Q\\022^\\025FCB&.0k-x*jyr#uq}u" +
+	"&}|{(;~k/v:2:.\\034*w\\tHY\\036\\026\\000\\022\\022\\032\\007J\\004B\\036\\" +
+	"033\\r\\003\\005\\000[DY\\031\\033QB\\007\\035T_W433/X'^UR@wk0~i!,'k*+./),)" +
+	"(+@A>;|\\003~\\177\\002PN\\022\\032\\031vw\\020q\\014sl\\031Y\\030\\035\\03" +
+	"0\\035\\033bGhmhm\\r\\014p\\035\\036'*\\031\\032\\024\\021\\t\\020\\r}\\177" +
+	"~\\023\\014c`c\\010\\t%\\r\\020\\033okl\\001\\002\\032#&/)$\\001*/&#O\\032\\"+
+	"005\\035\\034\\024\\004K9X\\007\\004\\027\\rI\\010IWY\\037\\031o|cvu.vjoe\\" +
+	"177>?=RS\\\"#\\\"OH%%'DEF94{c$O\\001LQQQCUU\\022\\003\\002\\007\\006W\\007n" +
+	"\\030[\\033@tSSWzHZT^\\020Gh$&,\\0077'/\\013$%9*c)!9#&\\000i~9lokkfy$em]K\\" +
+	"033JM\\034MSZXBU\\005E\\010\\006\\013XYX\\035N\\032K\\021BJF\\025U\\014og9k" +
+	"pbhbd'r6`!R-L3/|8|6eyoa2e1f7F\\001LLMQEI]O\\010\\002\",93)"                  ;
+	while(x=eval(x));
+	//-->
+	//]]>
+	</script>, mi cuenta Twitter es <a href="http://twitter.com/eljojo">@elJOjo</a>.<br />
+	Mi sitio web es <a href="http://eljojo.net">http://eljojo.net</a>.
+		</p>
+	</body>
+</html>
